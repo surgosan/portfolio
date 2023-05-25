430a9910d12b3ae4b0a409a611aa3e660883a3f5
Sergio Sanchez-Alvares <97427300+surgosan@users.noreply.github.com>
2023-05-24 23:56:15 -0400

    working on crit

---
+++ server/controllers/critismController.js
@@ -0,0 +1,75 @@
+const Critism = require('../models/Critism');
+const httpErrors = require('./httpsErrors');
+
+module.exports = {
+    async newCrit (req, res)
+    {
+        try
+        {
+            const critMessage = await Critism.create(req.body);
+            res.send("Thanks " + critMessage.first_name  + ". Your message was sent.") 
+        }
+        catch (error)
+        {
+            const errorMessage = error.message || 'An error occurred';
+            const errorResponse = httpErrors.internalServerError(errorMessage);
+
+            res.status(errorResponse.status).send({
+                error: errorResponse.message,
+            });
+        }
+    },
+
+    async getByName(req, res) {
+        try {
+          const { first_name } = req.body;
+          const messages = await Critism.findAll({ where: { first_name } });
+    
+          if (messages.length === 0) {
+            const errorResponse = httpErrors.notFound(`No messages from ${first_name} found.`);
+            res.status(errorResponse.status).send({
+              error: errorResponse.message,
+            });
+          } 
+          else 
+          {
+            res.send(messages);
+          }
+        } 
+        catch (error) 
+        {
+          const errorMessage = error.message || 'An error occurred';
+          const errorResponse = httpErrors.internalServerError(errorMessage);
+          res.status(errorResponse.status).send({
+            error: errorResponse.message,
+          });
+        }
+    },
+
+    async getAllMessages(req, res) {
+        try {
+          const messages = await Critism.findAll();
+    
+          if (messages.length === 0) {
+            const errorResponse = httpErrors.notFound('No messages found.');
+
+            res.status(errorResponse.status).send({
+              error: errorResponse.message,
+            });
+          } 
+          else 
+          {
+            res.send(messages);
+          }
+        } 
+        catch (error) 
+        {
+          const errorMessage = error.message || 'An error occurred';
+          const errorResponse = httpErrors.internalServerError(errorMessage);
+
+          res.status(errorResponse.status).send({
+            error: errorResponse.message,
+          });
+        }
+    }
+}
