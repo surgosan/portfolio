24c74768e5300441a8bceaec649e3fac45c04bdf
Sergio Sanchez-Alvares <97427300+surgosan@users.noreply.github.com>
2023-06-28 04:24:21 -0400

    Visitor Function Update - Server Side

--- server/controllers/visitorCountController.js
+++ server/controllers/visitorCountController.js
@@ -1,14 +1,69 @@
-const { VisitorCount } = require('../models');
+const {VisitorCount} = require('../models');
 const httpErrors = require('./httpsErrors');
 
 module.exports = {
-  async newVisit(req, res) {
-    try {
-      const { lastVisit } = req.body;
-      console.log(lastVisit);
-      const visit = await VisitorCount.create({ lastVisit });
+	async newVisit(req, res) {
+		try {
+			const {lastVisit} = req.body;
+			console.log(lastVisit);
+			const visit = await VisitorCount.create({lastVisit});
 
-      res.send('New visitor on ' + lastVisit);
+			res.send('New visitor on ' + lastVisit);
+		} catch (error) {
+			const errorMessage = error.message || 'An error occurred';
+			const errorResponse = httpErrors.internalServerError(errorMessage);
+
+			res.status(errorResponse.status).send({
+				error: errorResponse.message,
+			});
+		}
+	},
+
+	async getAllVisits(req, res) {
+		try {
+			const count = await VisitorCount.count();
+			res.send({count: count.toString()});
+		} catch (error) {
+			const errorMessage = error.message || 'An error occurred';
+			const errorResponse = httpErrors.internalServerError(errorMessage);
+			res.status(errorResponse.status).send({
+				error: errorResponse.message,
+			});
+		}
+	},
+
+	async getRecentVisits(req, res) {
+		try {
+			const recentVisits = await VisitorCount.findAll({
+				order: [['lastVisit', 'DESC']],
+				limit: 10,
+			});
+
+			res.send(recentVisits);
+		} catch (error) {
+			const errorMessage = error.message || 'An error occurred';
+			const errorResponse = httpErrors.internalServerError(errorMessage);
+
+			res.status(errorResponse.status).send({
+				error: errorResponse.message,
+			});
+		}
+	},
+
+  async deleteMostRecentVisit(req, res) {
+    try {
+      const mostRecentVisit = await VisitorCount.findOne({
+        order: [['lastVisit', 'DESC']],
+        raw: true,
+      });
+
+      if (!mostRecentVisit) {
+        return res.send('No visits found');
+      }
+
+      await VisitorCount.destroy({ where: { id: mostRecentVisit.id } });
+
+      res.send('Most recent visit deleted');
     } catch (error) {
       const errorMessage = error.message || 'An error occurred';
       const errorResponse = httpErrors.internalServerError(errorMessage);
@@ -17,18 +72,5 @@ module.exports = {
         error: errorResponse.message,
       });
     }
-  },
-
-  async getAllVisits(req, res) {
-    try {
-      const count = await VisitorCount.count();
-      res.send({ count: count.toString() });
-    } catch (error) {
-      const errorMessage = error.message || 'An error occurred';
-      const errorResponse = httpErrors.internalServerError(errorMessage);
-      res.status(errorResponse.status).send({
-        error: errorResponse.message,
-      });
-    }
-  },
+  }
 };
--- server/routes.js
+++ server/routes.js
@@ -4,19 +4,21 @@ const critismController = require('./controllers/critismController');
 const visitorController = require('./controllers/visitorCountController');
 
 module.exports = (app) => {
-    app.post('/message/new', messageController.newMessage);
-    app.post('/message/getByName', messageController.getByName);
-    app.get('/message/getAll', messageController.getAllMessages);
+	app.post('/message/new', messageController.newMessage);
+	app.post('/message/getByName', messageController.getByName);
+	app.get('/message/getAll', messageController.getAllMessages);
 
-    app.post('/critism/new', critismController.newCrit);
-    app.get('/critism/getAll', critismController.getAllMessages);
+	app.post('/critism/new', critismController.newCrit);
+	app.get('/critism/getAll', critismController.getAllMessages);
 
-    app.post('/suggestion/new', suggestionController.newSuggestion);
-    app.post('/suggestion/getByName', suggestionController.getSuggestionByFirstName);
-    app.get('/suggestion/getAll', suggestionController.getAllSuggestions);
-    app.put('/suggestion/fulfillById', suggestionController.fulfillSuggestionByID);
-    app.delete('/suggestion/deleteById/:id', suggestionController.deleteSuggestionByID);
+	app.post('/suggestion/new', suggestionController.newSuggestion);
+	app.post('/suggestion/getByName', suggestionController.getSuggestionByFirstName);
+	app.get('/suggestion/getAll', suggestionController.getAllSuggestions);
+	app.put('/suggestion/fulfillById', suggestionController.fulfillSuggestionByID);
+	app.delete('/suggestion/deleteById/:id', suggestionController.deleteSuggestionByID);
 
-    app.post('/visit/new', visitorController.newVisit);
-    app.get('/visit/getAll', visitorController.getAllVisits);
+	app.post('/visit/new', visitorController.newVisit);
+	app.get('/visit/getAll', visitorController.getAllVisits);
+	app.get('/visit/getRecent', visitorController.getRecentVisits);
+	app.delete('/visit/deleteRecent', visitorController.deleteMostRecentVisit);
 }
